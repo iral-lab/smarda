8fdc4fe143d913e1610f8277e409f3a26ed987ac
Grace Kathryn Chandler <gchan1@umbc.edu>
2015-11-11 22:11:00 -0500

    Added dosing check to the Color_match driver file

--- Color_Match.py
+++ Color_Match.py
@@ -10,13 +10,14 @@ import sys
 
 #a function will be added here to map RGB values (in a range) to a color name.
 
-#this dictionary will act as a representation for a 5 column table: Color, Medicine name, Brand name, Medicine class, (and in the real DB table ID). I have left out ID in this case beause it isn't particularly useful
+#this dictionary will act as a representation for a 5 column table: Color, Medicine name, Brand name, Medicine class, mg, max daily dosage, amount taken(and in the real DB table ID). I have left out ID in this case beause it isn't particularly useful
 colorMatch = dict()
 
 #initialize the colors we already have
-colorMatch["pink"] = ["Acetaminophen",["Tylenol", "Paracetamol" , "Panadol", "Mapap"], ["pain", "fever"]]
-colorMatch["blue"] = ["Guaifenesin", ["Mucinex", "Tussin", "Robitussin Chest Congestion", "Humibid"], ["cough","cold"]]
-colorMatch["orange"] = ["Diazepam", ["Valium", "Diastat", "Diastat AcuDial", "Diazepam Intensol"],["generalized anxiety", "panic", "phobias"]]
+colorMatch["pink"] = ["Acetaminophen",["Tylenol", "Paracetamol" , "Panadol", "Mapap"], ["pain", "fever"],325,3250,0]
+colorMatch["blue"] = ["Guaifenesin", ["Mucinex", "Tussin", "Robitussin Chest Congestion", "Humibid"], ["cough","cold"],300, 2400,0]
+colorMatch["orange"] = ["Diazepam", ["Valium", "Diastat", "Diastat AcuDial", "Diazepam Intensol"],["generalized anxiety", "panic", "phobias"],5,60,0]
+
 
 '''
     There will be more functionality added concerning drug interactions. For the sake of simplicity right now, we have chosen 3 very 
@@ -44,6 +45,19 @@ def main():
     print "Uses:"
     for use in colorMatch[color][2]:
         sys.stdout.write(use +  ",")
-
-
+        
+    #requesting medicine. Since we aren't using generated input, I arbitrarily chose the 'color' variable'
+    medicine = color
+    dose_request = 12
+    
+    #this variable keeps track of whether or not they have hit the closest dosage they can get to the maximum
+    max_dosage = False
+    while max_dosage != True and dose_request != 0:
+        if colorMatch[medicine][-1]+ (colorMatch[medicine][-3]) < colorMatch[medicine][-2]:
+            print("Dose Delivered")
+            colorMatch[medicine][-1]+= (colorMatch[medicine][-3])
+            dose_request -= 1
+        else: 
+            print("You have reached your maximum dosage. Please consult a physician if you are still experiencing unpleasant symptoms")
+            max_dosage = True
 main()
